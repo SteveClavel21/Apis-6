cea6fa0acaf658293945f465efb0789998bdf126
German Steven Méndez Clavel <clavelsteven441@gmail.com>
2024-09-10 18:07:57 -0600

    inicio

---
+++ API6/src/main/java/com/GSMC/Api6Application.java
@@ -0,0 +1,13 @@
+package com.GSMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api6Application.class, args);
+	}
+
+}
